f3a589e74a9045e9c4065fedb01d4278867ed9df
Olamieee <alongeola16@gmail.com>
2025-03-18 23:25:49 +0100

    school_name to school in models

--- recommenderApp/models.py
+++ recommenderApp/models.py
@@ -17,7 +17,7 @@ class TeacherProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.full_name} - {self.school_name}"
+        return f"{self.full_name} - {self.school}"
     
 class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='student_profile')
